ac81c305bec0d432d44d8eced1dede8f4f8f3df5
rebecaprg <rebeca.priorg@gmail.com>
2026-04-21 11:48:07 +0200

    fix model path

--- api/app.py
+++ api/app.py
@@ -1,50 +1,27 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import joblib
 import numpy as np
-import os
+from pathlib import Path
+import joblib
 
-# Cargar el modelo
-kmeans_pipeline = joblib.load("models/kmeans.pkl")
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_PATH = BASE_DIR / "models" / "kmeans.pkl"
 
 kmeans_pipeline = joblib.load(MODEL_PATH)
 
 app = FastAPI(title="Customer Segmentation API")
 
 
-# Input
 class Customer(BaseModel):
     age: float
     income: float
     spending_score: float
 
 
-# Preprocess (input) 
 def preprocess(data: Customer):
-
-    return np.array([[
-        data.age,
-        data.income,
-        data.spending_score
-    ]])
+    return np.array([[data.age, data.income, data.spending_score]])
 
 
-# Interpretacion de clusters 
-def interpret_cluster(cluster):
-
-    mapping = {
-        0: "Moderate Customers",
-        1: "High Value Customers",
-        2: "Young High Spenders",
-        3: "Average Customers",
-        4: "Low Spenders High Income",
-        5: "Low Value Customers"
-    }
-
-    return mapping.get(cluster, "Unknown")
-
-
-# Endpoints
 @app.get("/")
 def home():
     return {"message": "API running 🚀"}
@@ -52,12 +29,6 @@ def home():
 
 @app.post("/predict")
 def predict(customer: Customer):
-
     X = preprocess(customer)
-
     cluster = int(kmeans_pipeline.predict(X)[0])
-
-    return {
-        "cluster": cluster,
-        "segment": interpret_cluster(cluster)
-    }
+    return {"cluster": cluster}
